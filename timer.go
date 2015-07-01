f589a3277660f8858c1dd81d3812751d986fabbb
Ian Pye <pye@cloudhelix.com>
2015-07-01 14:04:09 -0700

    Adding a clear function for timer

--- timer.go
+++ timer.go
@@ -7,6 +7,7 @@ import (
 
 // Timers capture the duration and rate of events.
 type Timer interface {
+	Clear()
 	Count() int64
 	Max() int64
 	Mean() float64
@@ -81,6 +82,9 @@ type NilTimer struct {
 	m Meter
 }
 
+// Clear is a noop.
+func (NilTimer) Clear() {}
+
 // Count is a no-op.
 func (NilTimer) Count() int64 { return 0 }
 
@@ -143,6 +147,11 @@ type StandardTimer struct {
 	histogram Histogram
 	meter     Meter
 	mutex     sync.Mutex
+}
+
+// Clear clears underlaying histogram.
+func (t *StandardTimer) Clear() {
+	t.histogram.Clear()
 }
 
 // Count returns the number of events recorded.
@@ -253,6 +262,11 @@ func (t *StandardTimer) Variance() float64 {
 type TimerSnapshot struct {
 	histogram *HistogramSnapshot
 	meter     *MeterSnapshot
+}
+
+// Clear panics.
+func (t *TimerSnapshot) Clear() {
+	panic("Clear called on a TimerSnapshot")
 }
 
 // Count returns the number of events recorded at the time the snapshot was
